496442fe2924ad499b24049d672b94353818e694
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-04-22 13:25:19 -0700

    Completed content insertion of MVP, moving to styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent.js'
+import HeaderTitle from './HeaderTitle.js'
+import  ImageThumbnail from './ImageThumbnail.js';
+
+function HeaderContainer() {
+    return (
+        <div class="header">
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />    
+        </div>
+    )
+} 
+
+export default HeaderContainer 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function headertitle () {
+    return (
+        <div className="title">
+            <h2>Lambda School</h2>
+            <h2>@LambdaSchool</h2>
+            <h2>- 1 Jan</h2>
+        </div>
+    )
+}
+
+export default headertitle
