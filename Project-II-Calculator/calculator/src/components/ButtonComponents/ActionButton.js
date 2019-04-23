aa5b3b63e111df93d3f12816f428de6a8a98603d
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-04-23 12:58:59 -0700

    Completed MVP javascript, now styling

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+function ActionButton () {
+    return (
+        <div className="actions">
+            <button>&#247;</button>
+            <button>&#215;</button>
+            <button>&#8722;</button>
+            <button>&#43;</button>
+            <button>&#61;</button>
+        </div>
+    )
+};
+
+export default ActionButton;
